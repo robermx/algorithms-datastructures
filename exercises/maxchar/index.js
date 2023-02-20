b6f8c00c1178ac9e0a08f2b78d993a0a3b880954
Roberto Arenas <roberto.arenas2@icloud.com>
2023-02-20 14:24:31 -0600

    maxChar exercise complete

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charObj = {};
+
+  /* 
+  let max = 0;
+  let maxiChar = ''; 
+  */
+
+  for (const letter of str) {
+    charObj[letter] = (charObj[letter] || 0) + 1;
+  }
+
+  /* 
+  for (const char in charObj) {
+    if (charObj[char] > max) {
+      max = charObj[char];
+      maxiChar = char;
+    }
+  }
+
+  return maxiChar; 
+  */
+
+  return Object.keys(charObj).reduce((a, b) => (charObj[a] > charObj[b] ? a : b));
+}
 
 module.exports = maxChar;
